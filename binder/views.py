1a68ebeecaf3466ad0808b8ead65beb9ba3e453d
Kees Kluskens <kees@webduck.nl>
2016-08-15 16:19:44 +0200

    Fix `log_request_body` failing on Django 1.10 (#8)
    Fix compatibility with Django 1.10

--- binder/views.py
+++ binder/views.py
@@ -156,7 +156,7 @@ class ModelView(View):
 		# So we're good for most requests. For file uploads, we proceed without body logging.
 		if not self.log_request_body:
 			body = ' censored.'
-		elif not hasattr(request, '_body') and request._read_started:
+		elif request.method in ('HEAD', 'GET'):
 			body = ' unavailable.'
 		else:
 			if request.META.get('CONTENT_TYPE', '').lower() == 'application/json':
@@ -170,7 +170,7 @@ class ModelView(View):
 		try:
 			#### START TRANSACTION
 			with transaction.atomic():
-				if not kwargs.pop('unauthenticated', False) and not request.user.is_authenticated():
+				if not kwargs.pop('unauthenticated', False) and not request.user.is_authenticated:
 					raise BinderNotAuthenticated()
 
 				if 'method' in kwargs:
@@ -1174,7 +1174,7 @@ def debug_changesets_24h(request):
 	if request.method != 'GET':
 		raise BinderMethodNotAllowed()
 
-	if not request.user.is_authenticated():
+	if not request.user.is_authenticated:
 		logger.warning('Not authenticated.')
 		return HttpResponseForbidden('Not authenticated.')
 
