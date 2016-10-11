f50f034a718d7d6a88d3fd3da16cff077033e1b0
Peter Bex <peter@codeyellow.nl>
2016-10-11 11:02:10 +0200

    Add basic tests for "with" and "order_by"
    Ref #26

--- tests/test_model_view_basics.py
+++ tests/test_model_view_basics.py
@@ -4,7 +4,7 @@ import json
 from binder.json import jsonloads
 from django.contrib.auth.models import User
 
-from .testapp.models import Animal
+from .testapp.models import Animal, Zoo
 
 class ModelViewBasicsTest(TestCase):
 	def setUp(self):
@@ -43,3 +43,141 @@ class ModelViewBasicsTest(TestCase):
 		data = result['data']
 		self.assertEqual(data.get('id'), daffy.pk)
 		self.assertEqual(data.get('name'), 'Daffy Duck')
+
+
+	def test_get_model_with_invalid_id_sets_correct_code(self):
+		response = self.client.get('/animal/1234/')
+
+		self.assertEqual(response.status_code, 404)
+
+		result = jsonloads(response.content)
+		self.assertEqual('NotFound', result['code'])
+
+
+	def test_get_collection_with_no_models_returns_empty_array(self):
+		response = self.client.get('/animal/')
+
+		self.assertEqual(response.status_code, 200)
+
+		result = jsonloads(response.content)
+		self.assertEqual([], result['data'])
+
+
+	def test_get_collection_sorting(self):
+		gaia = Zoo(name='GaiaZOO')
+		gaia.full_clean()
+		gaia.save()
+
+		emmen = Zoo(name='Wildlands Adventure Zoo Emmen')
+		emmen.full_clean()
+		emmen.save()
+
+		coyote = Animal(name='Wile E. Coyote', zoo=gaia)
+		coyote.full_clean()
+		coyote.save()
+
+		roadrunner = Animal(name='Roadrunner', zoo=gaia)
+		roadrunner.full_clean()
+		roadrunner.save()
+
+		woody = Animal(name='Woody Woodpecker', zoo=emmen)
+		woody.full_clean()
+		woody.save()
+
+		response = self.client.get('/animal/', data={'order_by': 'name'})
+
+		self.assertEqual(response.status_code, 200)
+
+		result = jsonloads(response.content)
+		self.assertEqual(3, len(result['data']))
+		self.assertEqual('Roadrunner', result['data'][0]['name'])
+		self.assertEqual('Wile E. Coyote', result['data'][1]['name'])
+		self.assertEqual('Woody Woodpecker', result['data'][2]['name'])
+
+		# Reverse sorting
+		response = self.client.get('/animal/', data={'order_by': '-name'})
+
+		self.assertEqual(response.status_code, 200)
+
+		result = jsonloads(response.content)
+		self.assertEqual(3, len(result['data']))
+		self.assertEqual('Woody Woodpecker', result['data'][0]['name'])
+		self.assertEqual('Wile E. Coyote', result['data'][1]['name'])
+		self.assertEqual('Roadrunner', result['data'][2]['name'])
+
+
+		# Sorting by related field and then the main field
+		response = self.client.get('/animal/', data={'order_by': 'zoo.name,name'})
+
+		self.assertEqual(response.status_code, 200)
+
+		result = jsonloads(response.content)
+		self.assertEqual(3, len(result['data']))
+		self.assertEqual('Roadrunner', result['data'][0]['name'])
+		self.assertEqual('Wile E. Coyote', result['data'][1]['name'])
+		self.assertEqual('Woody Woodpecker', result['data'][2]['name'])
+
+
+		response = self.client.get('/animal/', data={'order_by': '-zoo.name,name'})
+
+		self.assertEqual(response.status_code, 200)
+
+		result = jsonloads(response.content)
+		self.assertEqual(3, len(result['data']))
+		self.assertEqual('Woody Woodpecker', result['data'][0]['name'])
+		self.assertEqual('Roadrunner', result['data'][1]['name'])
+		self.assertEqual('Wile E. Coyote', result['data'][2]['name'])
+
+		# Sorting by nonexistent field errors
+		response = self.client.get('/animal/', data={'order_by': 'zoo__name'})
+
+		self.assertEqual(response.status_code, 418)
+		result = jsonloads(response.content)
+		self.assertEqual('RequestError', result['code'])
+
+
+	def test_get_collection_with_relations(self):
+		gaia = Zoo(name='GaiaZOO')
+		gaia.full_clean()
+		gaia.save()
+
+		emmen = Zoo(name='Wildlands Adventure Zoo Emmen')
+		emmen.full_clean()
+		emmen.save()
+
+		coyote = Animal(name='Wile E. Coyote', zoo=gaia)
+		coyote.full_clean()
+		coyote.save()
+
+		roadrunner = Animal(name='Roadrunner', zoo=gaia)
+		roadrunner.full_clean()
+		roadrunner.save()
+
+		woody = Animal(name='Woody Woodpecker', zoo=emmen)
+		woody.full_clean()
+		woody.save()
+
+		# Quick check that relations are excluded unless we ask for them
+		response = self.client.get('/animal/', data={'order_by': 'name'})
+		self.assertEqual(response.status_code, 200)
+		self.assertIsNone(response.get('with_mapping'))
+		self.assertIsNone(response.get('with'))
+
+
+		response = self.client.get('/animal/', data={'order_by': 'name', 'with': 'zoo'})
+		self.assertEqual(response.status_code, 200)
+
+		result = jsonloads(response.content)
+		self.assertEqual(3, len(result['data']))
+		self.assertEqual(3, len(result['with_mapping']['zoo']))
+		self.assertEqual(2, len(result['with']['zoo']))
+		# TODO: Add test for relations with different name than models
+		self.assertEqual('zoo', result['with_mapping']['zoo'])
+
+		self.assertEqual(gaia.pk, result['data'][0]['zoo'])
+		zoo_by_id = {zoo['id']: zoo for zoo in result['with']['zoo']}
+		self.assertEqual('GaiaZOO', zoo_by_id[gaia.pk]['name'])
+		self.assertEqual('Wildlands Adventure Zoo Emmen', zoo_by_id[emmen.pk]['name'])
+		self.assertSetEqual(set([coyote.pk, roadrunner.pk]),
+				    set(zoo_by_id[gaia.pk]['animals']))
+		self.assertSetEqual(set([woody.pk]), set(zoo_by_id[emmen.pk]['animals']))
