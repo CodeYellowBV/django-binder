0365889c1b4346ef122cbf2c483fedf637380709
stefan <stefan@codeyellow.nl>
2017-03-27 14:13:36 +0200

    Revert "redundant commit, because last commit made git cry."
    This reverts commit 1f9c80bf8be23b014e0f18384397faeaecdb1000.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from setuptools import find_packages, setup
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 	README = readme.read()
 
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
