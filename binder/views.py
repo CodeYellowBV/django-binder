3efff54d642e604a5baef88a518b5ed3710c7145
Daan van der Kallen <mail@daanvdk.com>
2017-06-23 14:08:55 +0200

    Fixed small code formatting issues.

--- binder/views.py
+++ binder/views.py
@@ -888,7 +888,8 @@ class ModelView(View):
 		data[self._model_name()] = body['data']
 
 		# Sort object values by model/id
-		objects, prefixes = {}, {}
+		objects = {}
+		prefixes = {}
 		for modelname, objs in data.items():
 			if not isinstance(objs, list):
 				raise BinderRequestError('with.{} value should be a list')
@@ -906,7 +907,8 @@ class ModelView(View):
 				if not isinstance(obj['id'], int):
 					raise BinderRequestError('non-numeric id in with.{}[{}]'.format(modelname, idx))
 
-				objects[(model, obj['id'])], prefixes[(model, obj['id'])] = obj, modelname+'['+str(idx)+'].'
+				objects[(model, obj['id'])] = obj
+				prefixes[(model, obj['id'])] = modelname + '[' + str(idx) + '].'
 
 		# Figure out dependencies
 		logger.info('Resolving dependencies for {} objects'.format(len(objects)))
@@ -1000,7 +1002,7 @@ class ModelView(View):
 				view._store(obj, values, request)
 			except BinderValidationError as e:
 				e.validation_errors = {
-					prefixes[(model, oid)]+f: error
+					prefixes[(model, oid)] + f: error
 					for f, error in e.validation_errors.items()
 				}
 				raise e
