3d3e6991b7a7bdd7c8259a197082695f1549b0f4
Marcel Moreaux <marcel@codeyellow.nl>
2017-07-06 16:02:49 +0200

    Added test-project so devvers can play with the API.

---
+++ tests/testapp/views/__init__.py
@@ -0,0 +1,6 @@
+# We have it all, from A to Z!
+from .animal import AnimalView
+from .caretaker import CaretakerView
+from .contact_person import ContactPersonView
+from .costume import CostumeView
+from .zoo import ZooView
