727a0e1983bd647d720cad6a80202de4ffa8068d
stefanmajoor <stefan@codeyellow.nl>
2018-09-26 14:31:57 +0200

    Add automated test discoverer to binder

---
+++ binder/__init__.py
@@ -0,0 +1,1 @@
+from .tests_discoverer import load_tests
---
+++ binder/tests_discoverer.py
@@ -0,0 +1,21 @@
+from django.test.runner import is_discoverable
+from django.apps import apps
+
+# This prevents discoverer from loading models and views, which will
+# cause all sorts of random failures.
+def load_tests(loader, tests, pattern):
+    print("DISCOVER TESTS")
+    for app in apps.get_app_configs():
+        app_path = app.path
+        test_dir = '{}/tests/'.format(app_path)
+        try:
+            if is_discoverable(test_dir):
+                tests.addTests(loader.discover(test_dir))
+                print("Discover tests for app ", app.verbose_name)
+        except AssertionError:
+            # This is thrown if the app is loaded from the venv. These can not be tested anyway
+            pass
+
+    return tests
+
+
