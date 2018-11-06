510a6506dcca2cfc49fee555a8fae2b7b83846fd
Peter Bex <peter@codeyellow.nl>
2018-11-06 16:50:31 +0100

    Add management commands for creating and deleting user auth tokens from the CLI

---
+++ binder/plugins/token_auth/management/commands/create_user_token.py
@@ -0,0 +1,30 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.utils.translation import ugettext as _
+from django.contrib.auth.models import User
+from django.db import transaction
+
+from binder.plugins.token_auth.models import Token
+
+class Command(BaseCommand):
+	help = _('Create a fresh token for the given user')
+
+	def add_arguments(self, parser):
+		parser.add_argument('username', type=str)
+		parser.add_argument('-k', '--keep-existing', action='store_true', help='Keep existing tokens (by default, the new token will replace any existing ones)')
+
+
+	@transaction.atomic
+	def handle(self, *args, **options):
+		try:
+			user = User.objects.get(username=options['username'])
+		except User.DoesNotExist:
+			raise CommandError(_('User with username "%s" does not exist') % options['username'])
+
+		if not options['keep_existing']:
+			Token.objects.filter(user=user).delete() # If any
+
+		token = Token(user=user)
+		token.save()
+
+		self.stdout.write(_("Generated token for user %(user)s: %(token)s.") % {'user': user, 'token': token.token})
+		self.stdout.write(_("User now has %(count)d token(s).") % {'count': Token.objects.filter(user=user).count()} )
