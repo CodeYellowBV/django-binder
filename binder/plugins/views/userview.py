3bd041dbdfd8c15921a4c00b412febf70a6c1b22
stefanmajoor <stefan@codeyellow.nl>
2018-11-13 16:10:24 +0100

    remove unused import from userview

--- binder/plugins/views/userview.py
+++ binder/plugins/views/userview.py
@@ -4,7 +4,6 @@ from abc import ABCMeta, abstractmethod
 
 from django.contrib import auth
 from django.contrib.auth import update_session_auth_hash, password_validation
-from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.core.exceptions import ValidationError
 from django.db.models import Q
