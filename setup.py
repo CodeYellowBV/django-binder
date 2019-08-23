06c94be43d36ced85c1bcb830c3183b50bdf4f8b
Peter Bex <peter@codeyellow.nl>
2019-08-23 09:08:02 +0200

    Bump version to 1.5.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name='django-binder',
-	version='1.4.1',
+	version='1.5.0',
 	package_dir={'binder':'binder'},
 	packages=find_packages(),
 	include_package_data=True,
