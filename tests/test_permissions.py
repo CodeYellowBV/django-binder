8bdf2552769e1c9727145ff3b1799babc0843246
stefanmajoor <stefan@codeyellow.nl>
2020-01-15 08:17:05 +0100

    Add more tests for the permission check on multiput

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -1,3 +1,4 @@
+import json
 from binder.json import jsondumps
 from django.test import TestCase, override_settings
 
@@ -6,7 +7,7 @@ from django.test import Client
 
 from tests.testapp.models import Country, City
 
-#
+
 class TestScoping(TestCase):
     def setUp(self):
         super().setUp()
@@ -18,7 +19,6 @@ class TestScoping(TestCase):
         u.save()
         u.groups.add(group)
 
-
         self.client = Client()
         r = self.client.login(username='testuser', password='test')
         self.assertTrue(r)
@@ -28,14 +28,12 @@ class TestScoping(TestCase):
             ('testapp.change_country', 'all')
         ]
     })
-    def test_delete_scoping_on_multiput(self):
+    def test_cannot_delete_on_multiput_without_delete_permission(self):
         country = Country.objects.create(name='Nederland')
         city1 = City.objects.create(country=country, name='Amsterdam')
         city2 = City.objects.create(country=country, name='Leeuwarden')
 
         # Now suppose Friesland is finally going to seperate
-
-        foo = self.client.get('/country/?with=cities')
 
         res = self.client.put('/country/', data=jsondumps({
             'data': [{
@@ -45,12 +43,70 @@ class TestScoping(TestCase):
             }]
         }))
 
-
         # This is not ok
         assert res.status_code != 200
+
+        content = json.loads(res.content)
+        assert 'testapp.delete_city' == content['required_permission']
+
 
         # City 2 still exists!
         city2.refresh_from_db()
 
+    @override_settings(BINDER_PERMISSION={
+        'testapp.view_country': [
+            ('testapp.change_country', 'all'),
+            ('testapp.delete_city', 'all')
+        ]
+    })
+    def test_delete_scoping_on_multiput_with_delete_permission(self):
+        country = Country.objects.create(name='Nederland')
+        city1 = City.objects.create(country=country, name='Amsterdam')
+        city2 = City.objects.create(country=country, name='Leeuwarden')
 
-        print(city2)
+        # Now suppose Friesland is finally going to seperate
+
+        res = self.client.put('/country/', data=jsondumps({
+            'data': [{
+                'id': country.pk,
+                'name': 'Nederland',
+                'cities': [city1.pk]
+            }]
+        }))
+
+        # This is not ok
+        assert res.status_code == 200
+
+        # City 2 must not exist. TODO: test this
+        with self.assertRaises(City.DoesNotExist):
+            city2.refresh_from_db()
+
+    @override_settings(BINDER_PERMISSION={
+        'testapp.view_country': [
+            ('testapp.change_country', 'all')
+        ]
+    })
+    def test_cannot_change_on_multiput_without_change_permission(self):
+        country = Country.objects.create(name='Nederland')
+        city1 = City.objects.create(country=country, name='Amsterdam')
+
+        # Now suppose Friesland is finally going to seperate
+
+        res = self.client.put('/country/', data=jsondumps({
+            'data': [{
+                'id': country.pk,
+                'name': 'Nederland',
+                'cities': [city1.pk]
+            }],
+            'with': {
+                'city': [{
+                    'id': city1.pk,
+                    'name': 'Rotterdam',
+                }]
+            }
+        }))
+
+        # This is not ok
+        assert res.status_code == 403
+        content = json.loads(res.content)
+        assert 'testapp.change_city' == content['required_permission']
