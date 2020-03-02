3f3855f2ff92d4f9fe0104986acae5c251b2b0a5
stefanmajoor <stefan@codeyellow.nl>
2020-03-02 16:31:03 +0100

    Fix old way to do websocket triggers as well

--- binder/websocket.py
+++ binder/websocket.py
@@ -1,47 +1,58 @@
 from django.conf import settings
+
 from .json import jsondumps
 import requests
 from requests.exceptions import RequestException
 
+
 class RoomController(object):
-	def __init__(self):
-		self.room_listings = []
+    def __init__(self):
+        self.room_listings = []
 
-	def register(self, superclass):
-		for view in superclass.__subclasses__():
-			if view.register_for_model and view.model is not None:
-				listing = getattr(view, 'get_rooms_for_user', None)
+    def register(self, superclass):
+        for view in superclass.__subclasses__():
+            if view.register_for_model and view.model is not None:
+                listing = getattr(view, 'get_rooms_for_user', None)
 
-				if listing and callable(listing):
-					self.room_listings.append(listing)
+                if listing and callable(listing):
+                    self.room_listings.append(listing)
 
-			self.register(view)
+            self.register(view)
 
-		return self
+        return self
 
-	def list_rooms_for_user(self, user):
-		rooms = []
+    def list_rooms_for_user(self, user):
+        rooms = []
 
-		for l in self.room_listings:
-			rooms += l(user)
+        for l in self.room_listings:
+            rooms += l(user)
 
-		return rooms
+        return rooms
 
 
 def trigger(data, rooms):
-	import pika
-	from pika import BlockingConnection
-	from django.conf import settings
-	import json
+    if 'rabbitmq' in getattr(settings, 'HIGH_TEMPLAR', {}):
+        import pika
+        from pika import BlockingConnection
+        import json
 
-	connection_credentials = pika.PlainCredentials(settings.HIGH_TEMPLAR['rabbitmq']['username'],
-												   settings.HIGH_TEMPLAR['rabbitmq']['password'])
-	connection_parameters = pika.ConnectionParameters(settings.HIGH_TEMPLAR['rabbitmq']['host'],
-													  credentials=connection_credentials)
-	connection = BlockingConnection(parameters=connection_parameters)
-	channel = connection.channel()
+        connection_credentials = pika.PlainCredentials(settings.HIGH_TEMPLAR['rabbitmq']['username'],
+                                                       settings.HIGH_TEMPLAR['rabbitmq']['password'])
+        connection_parameters = pika.ConnectionParameters(settings.HIGH_TEMPLAR['rabbitmq']['host'],
+                                                          credentials=connection_credentials)
+        connection = BlockingConnection(parameters=connection_parameters)
+        channel = connection.channel()
 
-	channel.basic_publish('hightemplar', routing_key='*', body=jsondumps({
-		'data': data,
-		'rooms': rooms,
-	}))
+        channel.basic_publish('hightemplar', routing_key='*', body=jsondumps({
+            'data': data,
+            'rooms': rooms,
+        }))
+    if getattr(settings, 'HIGH_TEMPLAR_URL', None):
+        url = getattr(settings, 'HIGH_TEMPLAR_URL')
+        try:
+            requests.post('{}/trigger/'.format(url), data=jsondumps({
+                'data': data,
+                'rooms': rooms,
+            }))
+        except RequestException:
+            pass
