9014ed2265b711e425c4a309e16634293b0b7c7f
Peter Bex <peter@codeyellow.nl>
2020-03-11 11:54:46 +0100

    Add a few basic tests for the loaded values mixin

---
+++ tests/plugins/test_loaded_values_mixin.py
@@ -0,0 +1,96 @@
+from django.test import TestCase
+from ..testapp.models import Animal, Zoo, Caretaker
+
+class LoadedValuesMixinTest(TestCase):
+	def test_old_values_after_initialization_are_identical_to_current_but_unchanged(self):
+		artis = Zoo(name='Artis')
+		artis.save()
+		gaia = Zoo(name='Gaia Zoo')
+		gaia.save()
+		caretaker = Caretaker(name='Henk')
+		caretaker.save()
+
+		scooby = Animal(name='Scooby Doo', zoo_of_birth=artis, zoo=gaia)
+
+		self.assertEqual('Scooby Doo', scooby.get_old_value('name'))
+		self.assertEqual(gaia.id, scooby.get_old_value('zoo'))
+		self.assertEqual(artis.id, scooby.get_old_value('zoo_of_birth'))
+
+		self.assertTrue(scooby.field_changed('zoo'))
+		self.assertTrue(scooby.field_changed('zoo_of_birth'))
+		self.assertTrue(scooby.field_changed('name'))
+		self.assertTrue(scooby.field_changed('name', 'zoo', 'zoo_of_birth'))
+
+		scooby.save()
+
+		self.assertEqual('Scooby Doo', scooby.get_old_value('name'))
+		self.assertEqual(gaia.id, scooby.get_old_value('zoo'))
+		self.assertEqual(artis.id, scooby.get_old_value('zoo_of_birth'))
+		
+		self.assertFalse(scooby.field_changed('zoo'))
+		self.assertFalse(scooby.field_changed('zoo_of_birth'))
+		self.assertFalse(scooby.field_changed('name'))
+		self.assertFalse(scooby.field_changed('name', 'zoo', 'zoo_of_birth'))
+
+
+	def test_old_values_after_change_are_marked_as_changed_and_old_values_returns_old_value(self):
+		artis = Zoo(name='Artis')
+		artis.save()
+		gaia = Zoo(name='Gaia Zoo')
+		gaia.save()
+		caretaker = Caretaker(name='Henk')
+		caretaker.save()
+
+		scooby = Animal(name='Scooby Doo', zoo_of_birth=artis, zoo=gaia)
+		scooby.save()
+
+		# He goes back to the old zoo
+		scooby.zoo=artis
+		scooby.name='Scoooooby Doooo'
+
+		self.assertEqual('Scooby Doo', scooby.get_old_value('name'))
+		self.assertEqual(gaia.id, scooby.get_old_value('zoo'))
+		self.assertEqual(artis.id, scooby.get_old_value('zoo_of_birth'))
+
+		self.assertTrue(scooby.field_changed('zoo'))
+		self.assertFalse(scooby.field_changed('zoo_of_birth'))
+		self.assertTrue(scooby.field_changed('name'))
+		self.assertTrue(scooby.field_changed('name', 'zoo', 'zoo_of_birth'))
+
+
+	def test_old_values_return_current_value_after_fresh_fetch_from_db(self):
+		artis = Zoo(name='Artis')
+		artis.save()
+		gaia = Zoo(name='Gaia Zoo')
+		gaia.save()
+		caretaker = Caretaker(name='Henk')
+		caretaker.save()
+
+		scooby = Animal(name='Scooby Doo', zoo_of_birth=artis, zoo=gaia)
+		scooby.save()
+
+		# Let's not use refresh_from_db, but ensure this is a 100%
+		# clean object
+		scooby = Animal.objects.get(id=scooby.id)
+
+		self.assertEqual('Scooby Doo', scooby.get_old_value('name'))
+		self.assertEqual(gaia.id, scooby.get_old_value('zoo'))
+		self.assertEqual(artis.id, scooby.get_old_value('zoo_of_birth'))
+
+		self.assertFalse(scooby.field_changed('zoo'))
+		self.assertFalse(scooby.field_changed('zoo_of_birth'))
+		self.assertFalse(scooby.field_changed('name'))
+		self.assertFalse(scooby.field_changed('name', 'zoo', 'zoo_of_birth'))
+
+		# He goes back to the old zoo
+		scooby.zoo=artis
+		scooby.name='Scoooooby Doooo'
+
+		self.assertEqual('Scooby Doo', scooby.get_old_value('name'))
+		self.assertEqual(gaia.id, scooby.get_old_value('zoo'))
+		self.assertEqual(artis.id, scooby.get_old_value('zoo_of_birth'))
+
+		self.assertTrue(scooby.field_changed('zoo'))
+		self.assertFalse(scooby.field_changed('zoo_of_birth'))
+		self.assertTrue(scooby.field_changed('name'))
+		self.assertTrue(scooby.field_changed('name', 'zoo', 'zoo_of_birth'))
