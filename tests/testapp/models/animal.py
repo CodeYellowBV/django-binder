9cb0c2f13cc9032c252d535d8fb993e0cfad3c67
knokko <knokogator@hotmail.com>
2020-12-02 10:42:07 +0100

    My first attempt to write tests for soring on the values of context-aware annotations

--- tests/testapp/models/animal.py
+++ tests/testapp/models/animal.py
@@ -9,9 +9,11 @@ class Concat(Func):
 	function = 'CONCAT'
 	output_field = models.TextField()
 
+
 # From the api docs: an animal with a name.
 class Animal(LoadedValuesMixin, BinderModel):
 	name = models.TextField(max_length=64)
+	age = models.IntegerField(default=5)
 	zoo = models.ForeignKey('Zoo', on_delete=models.CASCADE, related_name='animals', blank=True, null=True)
 	zoo_of_birth = models.ForeignKey('Zoo', on_delete=models.CASCADE, related_name='+', blank=True, null=True) # might've been born outside captivity
 	caretaker = models.ForeignKey('Caretaker', on_delete=models.PROTECT, related_name='animals', blank=True, null=True)
@@ -33,4 +35,8 @@ class Animal(LoadedValuesMixin, BinderModel):
 		prefixed_name = ContextAnnotation(lambda request: Concat(
 			Value(request.GET.get('animal_name_prefix', 'Sir') + ' '),
 			F('name'),
+		))
+
+		age_product = ContextAnnotation(lambda request: Value(
+			Value(request.GET.get('factor')) * F('age')
 		))
