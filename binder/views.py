6a96bef3a09ca33b83c0d266ed082b89f472954c
Jeroen van Riel <jeroen@codeyellow.nl>
2021-03-29 11:22:05 +0200

    Fix horrible typo

--- binder/views.py
+++ binder/views.py
@@ -2052,7 +2052,7 @@ class ModelView(View):
 
 
 	def multi_put(self, request):
-		logger.info('ACTIVATING THE MULTI-PUT!!!1!')
+		logger.info('ACTIVATING THE MULTI-PUT!!!!!')
 
 		# Hack to communicate to _store() that we're not interested in
 		# the new data (for perf reasons).
