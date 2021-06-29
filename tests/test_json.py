3f5f1f34297b894d601fae7973c07d4e72a83e27
Bob Booy Liewes <bob@codeyellow.nl>
2021-06-29 16:10:29 +0200

    add test for all non default bounds datetimezonerange

--- tests/test_json.py
+++ tests/test_json.py
@@ -96,8 +96,20 @@ class JsonTest(TestCase):
 		d = DateTimeTZRange(empty=True)
 		self.assertEqual('[null]', binder_json.jsondumps(d))
 
-	def test_error_bounds_datetimerange_not_implimented(self):
+	def test_error_bounds_datetimerange_not_default_1(self):
 		t = datetime(2016, 1, 1, 1, 2, 3, 313337, tzinfo=timezone.utc)
 		d = DateTimeTZRange(t, t, bounds='()')
+		with self.assertRaises(BinderNotImplimented):
+			binder_json.jsondumps(d)
+
+	def test_error_bounds_datetimerange_not_default_2(self):
+		t = datetime(2016, 1, 1, 1, 2, 3, 313337, tzinfo=timezone.utc)
+		d = DateTimeTZRange(t, t, bounds='[]')
+		with self.assertRaises(BinderNotImplimented):
+			binder_json.jsondumps(d)
+
+	def test_error_bounds_datetimerange_not_default_3(self):
+		t = datetime(2016, 1, 1, 1, 2, 3, 313337, tzinfo=timezone.utc)
+		d = DateTimeTZRange(t, t, bounds='(]')
 		with self.assertRaises(BinderNotImplimented):
 			binder_json.jsondumps(d)
