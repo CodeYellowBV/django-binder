8545e81cea1e694d3fbecf65fca2c02a0124d6ea
Niklas <45883614+nstylo@users.noreply.github.com>
2021-07-26 13:28:45 +0200

    Fix typo

--- tests/testapp/models/timetable.py
+++ tests/testapp/models/timetable.py
@@ -1,6 +1,6 @@
 from binder.models import BinderModel, DateTimeRangeField
 
 
-# This is just a mock cladd to test DateTimeRangeField
+# This is just a mock class to test DateTimeRangeField
 class TimeTable(BinderModel):
 	daterange = DateTimeRangeField(null=True)
