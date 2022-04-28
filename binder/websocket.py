aa633901e3834a134eeeab7d148dea429dd066e9
knokko <knokogator@hotmail.com>
2022-04-28 17:27:47 +0200

    Try a BlockingConnection that runs on a background thread

--- binder/websocket.py
+++ binder/websocket.py
@@ -32,85 +32,47 @@ class RoomController(object):
         return rooms
 
 
-def singleton(get_instance):
-    instance = None
+use_channel_queue = None
+def use_channel(use_function):
+    global use_channel_queue
 
-    def get_singleton():
-        nonlocal instance
-        if instance is None:
-            instance = get_instance()
-        return instance
+    if use_channel_queue is None:
+        use_channel_queue = []
 
-    return get_singleton
+        def connection_thread_function():
+            import pika
+            connection_credentials = pika.PlainCredentials(
+                settings.HIGH_TEMPLAR['rabbitmq']['username'],
+                settings.HIGH_TEMPLAR['rabbitmq']['password'],
+            )
+            connection_parameters = pika.ConnectionParameters(
+                settings.HIGH_TEMPLAR['rabbitmq']['host'],
+                credentials=connection_credentials,
+            )
+            connection = pika.BlockingConnection(parameters=connection_parameters)
+            channel = connection.channel()
 
+            while True:
+                for use_function in use_channel_queue:
+                    use_function(channel)
+                use_channel_queue.clear()
+                connection.sleep(0.5)
 
-@singleton
-def get_channel():
-    log = ['Calling get_channel...']
-    import pika
-    connection_credentials = pika.PlainCredentials(
-        settings.HIGH_TEMPLAR['rabbitmq']['username'],
-        settings.HIGH_TEMPLAR['rabbitmq']['password'],
-    )
-    connection_parameters = pika.ConnectionParameters(
-        settings.HIGH_TEMPLAR['rabbitmq']['host'],
-        credentials=connection_credentials,
-    )
-    state = { 'value': 0 }
+        connection_thread = Thread(target=connection_thread_function)
+        connection_thread.setDaemon(True)
+        connection_thread.start()
 
-    def on_open():
-        log[0] += "Opened connection"
-        state['value'] = 1
-
-    def on_fail_open():
-        log[0] += "Failed to open connection"
-        state['value'] = -1
-
-    def on_close():
-        log[0] += "Closed connection"
-        state['value'] = -2
-
-    p_connection = [None]
-    
-
-    def test_start_loop():
-        connection = pika.SelectConnection(
-            parameters=connection_parameters,
-            on_open_callback=on_open,
-            on_open_error_callback=on_fail_open,
-            on_close_callback=on_close
-        )
-        p_connection[0] = connection
-        log[0] += "Starting ioloop..."
-        connection.ioloop.start()
-        log[0] += "Finished ioloop"
-    
-    log[0] += "Starting ioloop thread..."
-    Thread(target=test_start_loop).start()
-    log[0] += "Started ioloop thread"
-
-    counter = 0
-
-    while state['value'] == 0:
-        sleep(0.5)
-        counter += 1
-        if (counter == 10):
-            log[0] += 'final state is ' + str(state['value'])
-            raise RuntimeError('Failed to open pika SelectConnection: ' + log[0])
-
-    if state['value'] != 1:
-        raise RuntimeError('Failed to open pika SelectConnection: ' + str(state['value']))
-        # TODO Test this approach
-    return p_connection[0].channel()
-
+    use_channel_queue.append(use_function)
 
 def trigger(data, rooms):
     if 'rabbitmq' in getattr(settings, 'HIGH_TEMPLAR', {}):
-        channel = get_channel()
-        channel.basic_publish('hightemplar', routing_key='*', body=jsondumps({
-            'data': data,
-            'rooms': rooms,
-        }))
+        def use_function(channel):
+            channel.basic_publish('hightemplar', routing_key='*', body=jsondumps({
+                'data': data,
+                'rooms': rooms,
+            }))
+        use_channel(use_function)
+        
     if getattr(settings, 'HIGH_TEMPLAR_URL', None):
         url = getattr(settings, 'HIGH_TEMPLAR_URL')
         try:
