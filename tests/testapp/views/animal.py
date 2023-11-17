ba25a60655282dad9fce2cba8d272db1983c5376
Daan van der Kallen <daan.vdk@codeyellow.nl>
2023-11-17 09:58:32 +0100

    Stat from dict to namedtuple

--- tests/testapp/views/animal.py
+++ tests/testapp/views/animal.py
@@ -1,6 +1,6 @@
 from django.db.models import Count, Value
 
-from binder.views import ModelView
+from binder.views import ModelView, Stat
 
 from ..models import Animal
 
@@ -12,14 +12,12 @@ class AnimalView(ModelView):
 	transformed_searches = {'zoo_id': int}
 
 	stats = {
-		'without_caretaker': {
-			'expr': Count(Value(1)),
-			'filters': {
-				'caretaker:isnull': 'true',
-			},
-		},
-		'by_zoo': {
-			'expr': Count(Value(1)),
-			'group_by': 'zoo.name',
-		},
+		'without_caretaker': Stat(
+			Count(Value(1)),
+			filters={'caretaker:isnull': 'true'},
+		),
+		'by_zoo': Stat(
+			Count(Value(1)),
+			group_by='zoo.name',
+		),
 	}
